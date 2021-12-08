3604b13c99bb6d5d3c72fc8b2041d22e7d96e8e1
RaHughes <rogerallenhughes@hotmail.com>
2021-12-08 10:43:07 -0600

    Refactor to functional component

--- src/components/VideoGameList/VideoGameList.jsx
+++ src/components/VideoGameList/VideoGameList.jsx
@@ -1,28 +1,10 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
-class VideoGameList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            videoGames: []
-         }
-    }
-
-    componentDidMount(){
-        this.getVideoGames()
-    }
-
-    getVideoGames = async() => {
-        var response = await axios.get("https://localhost:44394/api/videogames")
-        this.setState({
-            videoGames: response.data
-        })
-    }
-
-    render() { 
+const VideoGameList = (props) => {
+ 
         return ( <div>
-            {this.state.videoGames.map(vg => {
+            {props.videoGames.map(vg => {
                 return <div key={Math.random()}>
                     <h1>{vg.title}</h1>
                     <h3>{vg.description}</h3>
@@ -32,6 +14,6 @@ class VideoGameList extends Component {
             })}
         </div> );
     }
-}
+
  
 export default VideoGameList;
