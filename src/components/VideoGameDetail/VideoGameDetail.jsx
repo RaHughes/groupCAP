6629fde298193c6f3ade9770fb352da0bbb055e8
RaHughes <rogerallenhughes@hotmail.com>
2021-12-10 14:03:13 -0600

    Add styling to VideoGameDetail

--- src/components/VideoGameDetail/VideoGameDetail.jsx
+++ src/components/VideoGameDetail/VideoGameDetail.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Link } from 'react-dom';
+import './VideoGameDetail.css'
 
 const VideoGameDetail = (props) => {
     return ( 
-        <div>
-            <h1>{props.videoGame.title}</h1>
-            <h3>{props.videoGame.description}</h3>
-            <h4>{props.videoGame.system}</h4>
-            <h4>{props.videoGame.price}</h4>
-            {props.user !== '' ? <button onClick={() => props.buyVideoGame()}>Buy Product</button> : <h4>You must be logged in to purchase this game</h4>}
+        <div className='detail'>
+            <h1>Title: {props.videoGame.title}</h1>
+            <h3>Description: {props.videoGame.description}</h3>
+            <h4>System: {props.videoGame.system}</h4>
+            <h4>Price: ${props.videoGame.price}</h4>
+            {props.user !== '' ? <button class='btn btn-secondary' onClick={() => props.buyVideoGame()}>Buy Product</button> : <h4>You must be logged in to purchase this game</h4>}
             {props.reviews.map(rv => {
                 if(props.videoGame.id === rv.videoGameId){
                   return(
-                    <div>
-                        <h4>Reviewer: {rv.user.firstName} {rv.user.lastName}</h4>
+                    <div className='reviews'>
+                        <h5>Reviewer: {rv.user.firstName} {rv.user.lastName}</h5>
                         <p>Rating: {rv.rating}</p>
                         <p>Review: {rv.reviewChar}</p>
                     </div>
