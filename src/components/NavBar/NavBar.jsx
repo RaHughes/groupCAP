eba0198190923c90377df5a5af91e979e5258acc
RaHughes <rogerallenhughes@hotmail.com>
2021-12-10 13:32:51 -0600

    Added some styling to NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,41 +1,43 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { Nav, Navbar as BootNav } from 'react-bootstrap'
+import './NavBar.css'
 
 function NavBar( {user, logout} ) {
     return ( 
-    <nav>
+    <BootNav class="nav">
         {user && <h4>Welcome {user.firstName}</h4>} 
-        <ul>
+        <ul class='navList'>
             <li>
-                <Link to='/' > Home </Link>
+                <Link class="btn btn-secondary" to='/' > Home </Link>
             </li>
             {!user &&
             <React.Fragment>    
                 <li>
-                    <Link to='/Login' > Login </Link>
+                    <Link to='/Login' class="btn btn-secondary"> Login </Link>
                 </li>
                 <li>
-                    <Link to='/Register' > Register </Link>
+                    <Link to='/Register' class="btn btn-secondary" > Register </Link>
                 </li>
             </React.Fragment>}
             {user &&
             <React.Fragment>
                 <li>
-                    <Link to='/' onClick={() => logout()}> Logout </Link>
+                    <Link to='/' onClick={() => logout()} class="btn btn-secondary"> Logout </Link>
                 </li>
                 <li>
-                    <Link to='/Sell' > Sellers Page </Link>
+                    <Link to='/Sell' class="btn btn-secondary"> Sellers Page </Link>
                 </li>
                 <li>
-                    <Link to='/Cart' > Cart </Link>
+                    <Link to='/Cart' class="btn btn-secondary"> Cart </Link>
                 </li>
                 <li>
-                    <Link to ='/Review'> Review Page</Link>
+                    <Link to ='/Review' class="btn btn-secondary"> Review Page</Link>
                 </li>
             </React.Fragment>
             }
         </ul>
-    </nav> );
+    </BootNav> );
     }
  
 export default NavBar;
